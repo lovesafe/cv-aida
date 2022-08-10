0f40ccc6233afaa98387a7781a66d3bebf59d1c9
aida-rahma <aidarhma09@gmail.com>
2022-08-10 16:15:24 +0700

    haii

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './component/header/header'
 import Nav from './component/nav/nav'
@@ -6,16 +6,23 @@ import About from './component/about/about'
 import Experience from './component/experience/experience'
 import Portfolio from './component/portfolio/portfolio'
 import Contact from './component/contact/contact'
-import Footer from './component/footer/footer'
-
+import Footer from './component/footer/footer';
 
 const App = () => {
+const [apa, setApa] = useState('Halloooo!')
+const [salam, setSalam] = useState('')
+const [job, setJob] = useState('')
+
+const datas = {
+  apa, setApa, salam, setSalam, job, setJob
+}
+
     return (
       <Router>
         <>
           <Nav/>
           <Routes >
-            <Route exact path='/' element={<Header/>} />
+            <Route exact path='/' element={<Header datas={datas}/>} />
             <Route exact path='/about' element={<About/>} /> 
             <Route exact path='/experience' element={<Experience/>} />
             <Route exact path='/portfolio' element={<Portfolio/>} />
--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -4,24 +4,40 @@ import CTA from './CTA'
 import ME from '../../assets/me1.png'
 import HeaderSocials from './HeaderSocials'
 
-const header = () => {
+const header = ({ datas }) => { 
+    const { apa, setApa } = datas
+    const { job, setJob } = datas
+    const { salam, setSalam } = datas
+  
+  function halo(){
+    setSalam ("Hello Saya");
+  }
+  
+  function orang(){
+    setApa ("Aida Rahma");
+  }
+  
+  function kerja(){
+    setJob ("SOFTWARE ENGINER");
+  }
     return (
-        <header>
-            <div className="container header__container">
-                <h5>Hello I'm</h5>
-                <h1>Aida Rahma</h1>
-                <h5 className="text-light">Fullstack Developer</h5>
-                <CTA />
-                <HeaderSocials />
-
-                <div className="me">
-                    <img src={ME} alt="me" />
-                </div>
-
-                <a href="#contact" className='scroll__down'>Scroll Down</a>
-            </div>
-        </header>
+      <header>
+        <div className="container header__container">
+          <div className='title'>
+          <h4>{salam}</h4>
+          <h1>{apa}</h1>
+          <h3>{job}</h3>
+          </div>
+          <button className='btn btn-primary' onClick={() => {orang(); kerja(); halo();}}><i><b>Klik ini!</b></i></button>
+          <CTA />
+          <HeaderSocials />
+  
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+      </header>
     )
-}
+  }
 
 export default header
